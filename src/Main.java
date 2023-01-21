31257913d45884f064c40002dcfaaf6b97027e21
arturSzuta <artszuta@hotmail.com>
2023-01-21 10:21:53 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String x = String(56);
+
+        System.out.println(x);
     }
 }
