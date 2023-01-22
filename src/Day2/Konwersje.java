9c165cac762bf6b55b413a652152466a33ce7f5c
arturSzuta <artszuta@hotmail.com>
2023-01-22 10:58:31 +0100

    zadanie 1 + 2

---
+++ src/Day2/Konwersje.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class Konwersje {
+    public static void main(String[] args) {
+        // Jawna konwersja liczb zmiennoprzecinkowch na calowite
+        // -> tracimy info po przecinku
+
+        int firstVale = (int)123.456F;
+        long secondVale = (long)456.789;
+
+        //Automatyczna konwersja typow
+        long thirdVale = 123; //niejawna/automnatyczna konwersja
+
+        // double -> float -> long
+
+
+    }
+}
