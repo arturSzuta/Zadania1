431ce1b60e8338c3ff7d18497ad63a301291d1d9
arturSzuta <artszuta@hotmail.com>
2023-01-23 20:43:16 +0100

    Task 16 start

--- src/Day2/Task16.java
+++ src/Day2/Task16.java
@@ -1,4 +1,22 @@
 package Day2;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Task16 {
+    public static void main(String[] args) {
+        int[] array = {12, 7, 9};
+
+        int[] newArray = replacement(array);
+
+        System.out.println(Arrays.toString(newArray));
+    }
+    private static int[] replacement(int[] input) {
+        int sizeOfArray = input.length - 1;
+        int[] tmp = new int[sizeOfArray];
+        tmp[0] = input[sizeOfArray];
+        tmp[sizeOfArray] = input[0];
+
+        return tmp;
+    }
 }
