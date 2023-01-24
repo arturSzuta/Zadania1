05031a16a5a61b9e75e703599d92515f6917b8fb
arturSzuta <artszuta@hotmail.com>
2023-01-24 19:16:23 +0100

    Task 16 done

--- src/Day2/Task16.java
+++ src/Day2/Task16.java
@@ -5,18 +5,28 @@ import java.util.Arrays;
 
 public class Task16 {
     public static void main(String[] args) {
-        int[] array = {12, 7, 9};
+        int[] array = {12, 7, 9, 5, 6};
 
-        int[] newArray = replacement(array);
+        int[] newArray = replacementHard(array);
 
         System.out.println(Arrays.toString(newArray));
     }
     private static int[] replacement(int[] input) {
         int sizeOfArray = input.length - 1;
-        int[] tmp = new int[sizeOfArray];
-        tmp[0] = input[sizeOfArray];
-        tmp[sizeOfArray] = input[0];
+        int tmp;
+        tmp = input[0];
+        input[0] = input[sizeOfArray];
+        input[sizeOfArray] = tmp;
 
-        return tmp;
+        return input;
+    }
+    private static int[] replacementHard(int[] input) {
+        int sizeOfArray = input.length - 1;
+        int[] tmpArray = new int[sizeOfArray + 1];
+
+        for(int i = 0; i <= sizeOfArray; i++) {
+            tmpArray[i] = input[sizeOfArray - i];
+        }
+        return tmpArray;
     }
 }
