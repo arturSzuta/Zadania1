20dfcfd97c73aaed6fdf38cb2f70d79545528913
arturSzuta <artszuta@hotmail.com>
2023-02-11 16:27:29 +0100

    Day 3 update 2

--- src/JavaAdvanced/Day3/Collections/Main.java
+++ src/JavaAdvanced/Day3/Collections/Main.java
@@ -70,7 +70,7 @@ public class Main {
         Map<String, Float> employees = new HashMap<>();
         //dodawania elementów do mapy
         employees.put("John Dool", 3000.0f);
-        employees.put("Chao Di", 4500.0f);
+        employees.put("Chao Di", null);
         employees.put("Prasad D", 1000.0f);
         //usuwanie element z mapy o danym kluczu
         employees.remove("John Dool");
@@ -79,11 +79,42 @@ public class Main {
         for (String key : employees.keySet()) {
             System.out.println("Pracownik: " + key);
         }
-        for (float value : employees.values()) {
+        for (Float value : employees.values()) {
             System.out.println("Wypłata: "+ value);
         }
         for (Map.Entry<String, Float> pair : employees.entrySet()) {
             System.out.println("Pracownik-wypłata " + pair);
         }
+
+
+        System.out.println("******");
+        System.out.println("Linked HashMap");
+
+        Map<Integer, String> employeesAge = new LinkedHashMap<>();
+        //dodawania elementów do mapy
+        employeesAge.put(12, "John Dool");
+        employeesAge.put(32, "Chao Di");
+        employeesAge.put(34, "Prasad D");
+        employeesAge.keySet().forEach(System.out::println);
+        employeesAge.values().forEach(System.out::println);
+        employeesAge.entrySet().forEach(System.out::println);
+
+        if (employeesAge.containsKey(30)) {
+            System.out.println("Found");
+        } else {
+            System.out.println("Not found");
+        }
+
+        System.out.println(employeesAge.containsKey(30) ? "Found" : "Not found");
+
+        System.out.println("******");
+        System.out.println("TreeMap");
+        Map<Integer, Integer> sumOfDigits = new TreeMap<>();
+        sumOfDigits.put(44, 2);
+        sumOfDigits.put(34, 6);
+        sumOfDigits.put(23, 2);
+        sumOfDigits.put(6, 3);
+
+        sumOfDigits.entrySet().forEach(System.out::println);
     }
 }
